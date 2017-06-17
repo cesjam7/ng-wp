deaab9147546e3a878ad875d608e6473e63bc92e
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2017-06-17 01:00:42 -0500

    listo

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
                     <span class="sr-only">Toggle navigation</span>
                     Menu <i class="fa fa-bars"></i>
                 </button>
-                <a class="navbar-brand" href="#!/">NG WP {{ 2+2 }}</a>
+                <a class="navbar-brand" href="#!/">NG WP</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
